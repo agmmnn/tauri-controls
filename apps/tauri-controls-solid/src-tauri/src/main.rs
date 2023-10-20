e38bad2c17717d6830c7842d5d4c562490700e37
Annie Ehler <annie.ehler.4@gmail.com>
2023-10-20 12:26:30 -0700

    Update to new tauri api

--- apps/tauri-controls-solid/src-tauri/src/main.rs
+++ apps/tauri-controls-solid/src-tauri/src/main.rs
@@ -3,7 +3,6 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
--- apps/tauri-controls/src-tauri/src/main.rs
+++ apps/tauri-controls/src-tauri/src/main.rs
@@ -3,7 +3,6 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -16,10 +15,6 @@ fn main() {
         .invoke_handler(tauri::generate_handler![greet])
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
-            app.get_window("main").unwrap().open_devtools();
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
